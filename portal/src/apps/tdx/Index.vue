6d19f168aee7059001f2537ba38249ea4ea574ad
Lu Ken <ken.lu@intel.com>
2021-07-31 19:54:44 +0800

    mock up the perf data
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- portal/src/apps/tdx/Index.vue
+++ portal/src/apps/tdx/Index.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an TDX page</h1>
-    <div align="center">
-      <apexchart
-        type="radar"
-        height="500"
-        :options="chartOptions"
-        :series="series"
-      ></apexchart>
-    </div>
-  </div>
+  <el-tabs tab-position="left" tabstyle="height: 200px">
+    <el-tab-pane label="2021WW31">
+      <div>
+        <h1>2021WW31 Release</h1>
+        <div align="center">
+          <apexchart
+            type="radar"
+            height="500"
+            :options="chartOptions"
+            :series="series"
+          ></apexchart>
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="2021WW27">
+      <div>
+        <h1>2021WW27 Release</h1>
+        <div align="center">
+          <apexchart
+            type="radar"
+            height="500"
+            :options="chartOptions"
+            :series="series"
+          ></apexchart>
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="2021WW24">
+      <div>
+        <h1>2021WW24 Release</h1>
+        <div align="center">
+          <apexchart
+            type="radar"
+            height="500"
+            :options="chartOptions"
+            :series="series"
+          ></apexchart>
+        </div>
+      </div>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
@@ -22,13 +52,19 @@ export default {
           id: 'vuechart-example'
         },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          categories: ['1991', '1992', '1993', '1994', '1995', 1996, 1997, 1998]
         }
       },
-      series: [{
-        name: 'series-1',
-        data: [30, 40, 35, 50, 49, 60, 70, 91]
-      }]
+      series: [
+        {
+          name: 'series-1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91]
+        },
+        {
+          name: 'series-2',
+          data: [67, 22, 33, 6, 14, 60, 7, 13]
+        }
+      ]
     }
   }
 }
