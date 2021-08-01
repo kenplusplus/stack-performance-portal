9fdf239c9ab636df676b8adfb6af59371b0e3458
Lu Ken <ken.lu@intel.com>
2021-08-01 14:04:24 +0800

    add more description
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- portal/src/apps/home/Index.vue
+++ portal/src/apps/home/Index.vue
@@ -2,6 +2,12 @@
   <div>
     <img src="@/assets/logo.png" alt="Logo" />
     <h1>Welcome to Stack's Performance Portal</h1>
-    <a>Linux Cloud Stack Team</a>
+    <a href="mailto:lcse-linux.os.stack.engineering@intel.com">Linux Cloud Stack Team</a><br>
+    <a>Xie, May</a>
+    <p>
+      <a>
+        <i>PLEASE NOTE: The performance data is only used for internal, please do not share outside of Intel</i>
+      </a>
+    </p>
   </div>
 </template>
